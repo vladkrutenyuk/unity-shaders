04330837c7cacaf91959614f7492267a6cbae685
KVY <kvy@phygitalism.com>
2020-03-22 15:00:13 +0300

    Polygon Clipping

---
+++ Assets/Scripts/PolygonClipping.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PolygonClipping : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private Transform[] _vertex;
+
+    private void Start()
+    {
+        UpdateMaterial();
+    }
+
+    private void Update() 
+    {
+        UpdateMaterial();
+    }
+
+    private void UpdateMaterial()
+    {      
+        Vector4[] pointsVector4 = new Vector4[100];
+
+        for (int i = 0; i < _vertex.Length; i++)
+        {
+            pointsVector4[i] = new Vector2(_vertex[i].localPosition.x, _vertex[i].localPosition.z);
+        }
+
+        _meshRenderer.material.SetInt("_pointsCount", _vertex.Length);
+        _meshRenderer.material.SetVectorArray("_points", pointsVector4);
+    }
+}
