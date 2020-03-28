f63ab3100a44cdf673c0809788516865f818df52
KVY <kvy@phygitalism.com>
2020-03-28 13:05:24 +0300

    PostEffect for Standart pipeline

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostEffectScript : MonoBehaviour
+{
+    [SerializeField] Material _material;
+    public void OnRenderImage(RenderTexture src, RenderTexture dest) 
+    {
+        // WORKS ONLY ON STANDART RENDER PIPELINE !!!
+        Graphics.Blit(src, dest, _material);
+    }
+}
